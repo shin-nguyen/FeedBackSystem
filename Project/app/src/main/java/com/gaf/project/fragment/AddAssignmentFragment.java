db3cddbe3bd972df05720e799d89c12db0fc0249
Quyet Sinh <67093870+sinhnguyen2702@users.noreply.github.com>
2021-05-17 14:43:34 +0700

    add assignment and show dialog

--- Project/app/src/main/java/com/gaf/project/fragment/AddAssignmentFragment.java
+++ Project/app/src/main/java/com/gaf/project/fragment/AddAssignmentFragment.java
@@ -1,12 +1,19 @@
 package com.gaf.project.fragment;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.gaf.project.R;
 
@@ -16,6 +23,9 @@ import com.gaf.project.R;
  * create an instance of this fragment.
  */
 public class AddAssignmentFragment extends Fragment {
+
+    private View view;
+    private Button btnSave,btnBack;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,14 +40,6 @@ public class AddAssignmentFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddAssignmentFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static AddAssignmentFragment newInstance(String param1, String param2) {
         AddAssignmentFragment fragment = new AddAssignmentFragment();
@@ -61,6 +63,38 @@ public class AddAssignmentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.add_assignment, container, false);
+        view = inflater.inflate(R.layout.add_assignment, container, false);
+
+        final Spinner sprModule = (Spinner) view.findViewById(R.id.spinner_module);
+        String[] items_sprMail= new String[]{"sinh@gmail.com", "nguyen@gmail.com", "quyet@gmail.com"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter_sprMail = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, items_sprMail);
+        //set the spinners adapter to the previously created one.
+        sprModule.setAdapter(adapter_sprMail);
+
+        btnSave = view.findViewById(R.id.btn_save);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"save",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnBack= view.findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnBack.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        getActivity().onBackPressed();
+                    }
+                });
+                Toast.makeText(getContext(),"back",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
     }
 }
--- Project/app/src/main/java/com/gaf/project/fragment/AssignmentFragment.java
+++ Project/app/src/main/java/com/gaf/project/fragment/AssignmentFragment.java
@@ -1,5 +1,6 @@
 package com.gaf.project.fragment;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import android.view.LayoutInflater;
@@ -7,6 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import androidx.annotation.NonNull;
@@ -28,6 +31,8 @@ import com.gaf.project.model.Module;
 import com.gaf.project.model.Trainee;
 import com.gaf.project.model.Trainer;
 import com.gaf.project.model.TypeFeedback;
+import com.gaf.project.service.AssignmentService;
+import com.gaf.project.utils.ApiUtils;
 
 
 import java.security.Timestamp;
@@ -38,23 +43,25 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
-public class AssignmentFragment extends Fragment implements View.OnClickListener{
+public class AssignmentFragment extends Fragment{
 
     private View view;
     private NavController navigation;
+//    private AssignmentService assignmentService;
     private RecyclerView recyclerViewAssignment;
     private AssignmentAdapter assignmentAdapter;
     private List<Assignment> listAssignment;
-    private Button btnAddAssignment;
+    private Button btnAdd;
 
     public AssignmentFragment(){
 
     }
 
-    @Override
-    public void onClick(View v) {
-
-    }
+//    @Override
+//    public void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        assignmentService = ApiUtils.getAssignmentService();
+//    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -70,6 +77,18 @@ public class AssignmentFragment extends Fragment implements View.OnClickListener
         recyclerViewAssignment = view.findViewById(R.id.rcv_assignment);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(view.getContext());
         recyclerViewAssignment.setLayoutManager(linearLayoutManager);
+
+        assignmentAdapter = new AssignmentAdapter(new AssignmentAdapter.IClickItem() {
+            @Override
+            public void update(Assignment item) {
+                clickUpdate(item);
+            }
+
+            @Override
+            public void delete(Assignment item) {
+                clickDelete(item);
+            }
+        });
 
         listAssignment = new ArrayList<>();
 
@@ -87,22 +106,68 @@ public class AssignmentFragment extends Fragment implements View.OnClickListener
         Assignment assignment = new Assignment(assignmentId,"Ec");
         listAssignment.add(assignment);
 
-
-        assignmentAdapter = new AssignmentAdapter();
-        assignmentAdapter.setData(listAssignment,getContext());
+        assignmentAdapter.setData(listAssignment);
 
         recyclerViewAssignment.setAdapter(assignmentAdapter);
 
         navigation = Navigation.findNavController(view);
 
-        btnAddAssignment = view.findViewById(R.id.btn_add_assignment);
+        btnAdd = view.findViewById(R.id.btn_add_assignment);
 //        btnAddAssignment.setVisibility(View.GONE);//hide button
 //        btnAddAssignment.setVisibility(View.VISIBLE);//show button
-        btnAddAssignment.setOnClickListener(new View.OnClickListener() {
+        btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 navigation.navigate(R.id.action_nav_assignment_to_add_assignment_fragment);
             }
         });
+    }
+
+    private void clickUpdate(Assignment item) {
+//        Bundle bundle = new Bundle();
+        navigation.navigate(R.id.action_nav_assignment_to_edit_assignment_fragment);
+    }
+
+    private void clickDelete(Assignment item){
+        AlertDialog.Builder builder=new AlertDialog.Builder(getContext());
+        View viewBuilder=LayoutInflater.from(getContext()).inflate(R.layout.warning_dialog,null);
+
+        builder.setView(viewBuilder);
+
+        final AlertDialog warningDialog=builder.create();
+
+        TextView warningContent = viewBuilder.findViewById(R.id.txt_warning_content);
+        warningContent.setText("Do you want to delete this Assignment?");
+
+        warningDialog.show();
+
+        Button btnCancel = viewBuilder.findViewById(R.id.btn_cancel);
+        btnCancel.setOnClickListener(vi->{
+            warningDialog.dismiss();
+        });
+
+        Button btnYes = viewBuilder.findViewById(R.id.btn_yes);
+        btnYes.setOnClickListener(v->{
+            warningDialog.dismiss();
+
+            View viewBuilderSuccess=LayoutInflater.from(getContext()).inflate(R.layout.successful_dialog,null);
+
+            builder.setView(viewBuilderSuccess);
+
+            TextView warningSuccessContent = viewBuilderSuccess.findViewById(R.id.txt_success_dialog_message);
+            warningSuccessContent.setText("Delete Success!");
+
+            final AlertDialog successDialog=builder.create();
+            successDialog.show();
+
+            Button btnSuccessCancel = viewBuilderSuccess.findViewById(R.id.btn_success_confirm);
+            btnSuccessCancel.setOnClickListener(vi -> {
+                successDialog.dismiss();
+            });
+        });
+    }
+
+    public void showToast(String string){
+        Toast.makeText(getContext(),string,Toast.LENGTH_LONG).show();
     }
 }
