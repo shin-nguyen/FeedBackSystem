ddbb5ba90ee24c4376d022b183c9b8cda2956cb0
Hung Do <beekunfar@gmai.com>
2021-05-23 20:08:31 +0700

    change ip

--- Project/app/src/main/java/com/gaf/project/utils/ApiUtils.java
+++ Project/app/src/main/java/com/gaf/project/utils/ApiUtils.java
@@ -14,7 +14,7 @@ import com.gaf.project.service.TrainerService;
 public class ApiUtils {
 
     //Dia chi may host
-    public static final String BASE_URL = "http://192.168.1.14:8080/";
+    public static final String BASE_URL = "http://192.168.1.251:8080/";
 
     public static AuthenticationService getAuthenticationService() {
         return RetrofitConfig.getInstance().buildRetrofit().create(AuthenticationService.class);
