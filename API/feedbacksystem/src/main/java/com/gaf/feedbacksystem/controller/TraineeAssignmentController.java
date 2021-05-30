f932d7582327aa14f77d6ecfcb27dda067e148f9
TangYuCheng <42094883+thongchuthanh2000@users.noreply.github.com>
2021-05-31 00:05:32 +0700

    Fix Config

--- API/feedbacksystem/src/main/java/com/gaf/feedbacksystem/controller/TraineeAssignmentController.java
+++ API/feedbacksystem/src/main/java/com/gaf/feedbacksystem/controller/TraineeAssignmentController.java
@@ -33,36 +33,61 @@ public class TraineeAssignmentController {
     @Autowired
     private IAssignmentService assignmentService;
 
+//    @PreAuthorize("hasRole(\"" + SystemConstant.TRAINEE_ROLE + "\")")
+//    @PostMapping(value = "/{userid}/{code}")
+//    public TraineeAssignmentDto create(@PathVariable (name = "userid") String userid,
+//                                       @PathVariable (name = "code") String code){
+//        try{
+//            AssignmentDto assignmentDto = assignmentService.findByCode(code);
+//            TraineeDto traineeDto = traineeService.findByUserName(userid);
+//
+//            if (assignmentDto == null || traineeDto == null){
+//                throw new MyResourceNotFoundException();
+//            }
+//
+//            TraineeAssignmentDto traineeAssignmentDto = new TraineeAssignmentDto();
+//            traineeAssignmentDto.setAssignment(assignmentDto);
+//            traineeAssignmentDto.setTrainee(traineeDto);
+//
+//            return  traineeAssignmentService.save(traineeAssignmentDto);
+//        }
+//        catch (MyResourceNotFoundException exc) {
+//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Trainee_Assignment Not Found", exc);
+//        }
+//    }
+
+    @PostMapping(value = "/{username}/{code}")
     @PreAuthorize("hasRole(\"" + SystemConstant.TRAINEE_ROLE + "\")")
-    @PostMapping(value = "/{userid}/{code}")
     public Map<String, Boolean> create(@PathVariable (name = "username") String username,
-                                        @PathVariable (name = "code") String code) {
+                                       @PathVariable (name = "code") String code) {
         try {
             Map<String, Boolean> response = new HashMap<>();
-                AssignmentDto assignmentDto = assignmentService.findByCode(code);
-                TraineeDto traineeDto = traineeService.findByUserName(username);
+            AssignmentDto assignmentDto = assignmentService.findByCode(code);
+            TraineeDto traineeDto = traineeService.findByUserName(username);
 
-                if (traineeDto == null){
-                    throw new MyResourceNotFoundException();
-                }
-
-                if (assignmentDto == null){
-                    response.put("add", Boolean.FALSE);
-                }
-                else if (assignmentDto.getRegistrationCode().equals(code)){
-                    TraineeAssignmentDto traineeAssignmentDto = new TraineeAssignmentDto();
-                    traineeAssignmentDto.setAssignment(assignmentDto);
-                    traineeAssignmentDto.setTrainee(traineeDto);
-                }
-
-            if (assignmentDto == null || traineeDto == null){
+            if (traineeDto == null){
                 throw new MyResourceNotFoundException();
             }
 
-            return  response;
-        }
-        catch (MyResourceNotFoundException exc) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Trainee_Assignment Not Found", exc);
+            if (assignmentDto == null){
+                response.put("add", Boolean.FALSE);
+            }
+            else if (assignmentDto.getRegistrationCode().equals(code)){
+                TraineeAssignmentDto traineeAssignmentDto = new TraineeAssignmentDto();
+                traineeAssignmentDto.setAssignment(assignmentDto);
+                traineeAssignmentDto.setTrainee(traineeDto);
+
+                traineeAssignmentService.save(traineeAssignmentDto);
+
+                response.put("add", Boolean.TRUE);
+            }
+//                else {
+//                    response.put("add", Boolean.FALSE);
+//                }
+
+            return response;
+        } catch (MyResourceNotFoundException exc) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Classes Not Found", exc);
         }
     }
 }
