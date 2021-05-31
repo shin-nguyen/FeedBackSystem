a5c58357717fc4fe00dcae8f2c9401fcadccefe5
TangYuCheng <42094883+thongchuthanh2000@users.noreply.github.com>
2021-06-01 00:05:10 +0700

    Chay Dc ne

--- API/feedbacksystem/src/main/java/com/gaf/feedbacksystem/controller/CommentController.java
+++ API/feedbacksystem/src/main/java/com/gaf/feedbacksystem/controller/CommentController.java
@@ -19,7 +19,7 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping(value = "/class")
+@RequestMapping(value = "/comment")
 @Tag(name = "comment")
 public class CommentController {
     @Autowired
